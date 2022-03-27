e8be2d27ab72301850c8ac45285c16b5032b0d61
shenzm01 <50047170+shenzm01@users.noreply.github.com>
2022-03-27 10:30:19 +0800

    Update hitcarder.py
    详细化打卡信息更改提示

--- hitcarder.py
+++ hitcarder.py
@@ -195,7 +195,10 @@ def main(username, password):
     try:
         ret = hit_carder.check_form()
         if not ret:
-            return 2, '打卡信息已改变，请手动打卡'
+            res = self.sess.get(self.base_url)
+            html = res.content.decode()
+            new_form = re.findall(r'<ul>[\s\S]*?</ul>', html)[0]
+            return 2, '打卡信息已改变，请手动打卡', new_form
     except Exception as err:
         return 1, '获取信息失败，请手动打卡: ' + str(err)
 
